1e56c204a232ba0724f9ed1bdd9fe929a0dcc9b2
YIKUI ZHAI <yikuizhai@163.com>
2025-11-29 13:43:15 +0800

    Add files via upload

---
+++ tools/train.sh
@@ -0,0 +1,11 @@
+#python ./tools/train.py --file_root LEVIR --lr 5e-4 --max_steps 40000
+#
+#python ./tools/train.py --file_root BCDD --lr 5e-4 --max_steps 40000
+
+python /media/yang/1305D8BDB8D46DEE/zzz/A2Net-main/tools/train.py --file_root SYSU --lr 5e-4 --max_steps 40000
+
+python /media/yang/1305D8BDB8D46DEE/zzz/A2Net-main/tools/train_mokuai_1_xiaorong.py --file_root SYSU --lr 5e-4 --max_steps 40000
+
+#python /media/yang/1305D8BDB8D46DEE/zzz/A2Net-main/tools/train_mokuai_1_xiaorong.py --file_root SYSU --lr 5e-4 --max_steps 40000
+#
+#python /media/yang/1305D8BDB8D46DEE/zzz/A2Net-main/tools/train_mokuai_1_xiaorong.py --file_root SYSU --lr 5e-4 --max_steps 40000
